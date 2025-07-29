a691033d7b8e63900ff837d9e0077bba8b70ad97
Sheshadri Kumar <kvs24161@gmail.com>
2025-07-29 18:54:18 +0530

    DayNine01.java

--- Java-Task/Notepad++Prog/DayNine01.java
+++ Java-Task/Notepad++Prog/DayNine01.java
@@ -1,23 +1,23 @@
 // Move Non Zeros at the End
 
 
-// public class DayNine01{
-// public static void main(String[] args){
-// int a[]={1,0,2,0,4};
-// int nonzero=0;
-// for(int i=0;i<a.length;i++){
-// if(a[i]!=0){
-// a[nonzero++]=a[i];
-// }
-// }
-// while(nonzero<a.length){
-// a[nonzero++]=;
-// }
-// for(int i=0;i<a.length;i++){
-	// System.out.println(a[i]);
-// }
-// }
-// }
+public class DayNine01{
+public static void main(String[] args){
+int a[]={1,0,2,0,4};
+int nonzero=0;
+for(int i=0;i<a.length;i++){
+if(a[i]!=0){
+a[nonzero++]=a[i];
+}
+}
+while(nonzero<a.length){
+a[nonzero++]=;
+}
+for(int i=0;i<a.length;i++){
+	System.out.println(a[i]);
+}
+}
+}
 
 
 // Move Non Zeros at the Start..
